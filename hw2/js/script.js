974abd4025e331fb11ff31585a60f184c65ea0aa
Anna Pronina <kiisya1@yandex.ru>
2021-03-19 18:21:36 +0300

    Добавляет домашнее задание 3

--- hw2/js/script.js
+++ hw2/js/script.js
@@ -1,74 +1,118 @@
 'use strict';
 
-let textOutput = document.querySelector('.stack__output');
-let form = document.querySelector('.stack__form');
-let textInput = document.querySelector('.stack__input');
+(function () {
 
-let popButton = document.querySelector('#pop');
-let peekButton = document.querySelector('#peek');
-let getSizeButton = document.querySelector('#getSize');
+  let textOutput = document.querySelector('.stack__output');
+  let form = document.querySelector('.stack__form');
+  let textInput = document.querySelector('.stack__input');
+  let logOutput = document.querySelector('.log');
 
-// Реализация стека
+  let popButton = document.querySelector('#pop');
+  let peekButton = document.querySelector('#peek');
+  let getSizeButton = document.querySelector('#getSize');
 
-class Stack {
-  constructor() {
-    this.list = {};
-    this.length = 0;
+  // Логирование
+
+  let originalLog = console.log;
+
+  function getTime () {
+    let date = new Date();
+    let time = [
+      '0' + date.getHours(),
+      '0' + date.getMinutes(),
+      '0' + date.getSeconds(),
+  ].map(component => component.slice(-2));
+
+    return time.join(':');
   }
 
-  push(value) {
-    this.list[this.length] = value;
-    this.length++;
+  function addLogElement (message) {
+    let p = document.createElement('p');
+    p.className = 'log__message';
+    p.textContent = message;
+    logOutput.append(p);
   }
 
-  pop() {
-    if (this.length === 0) return undefined;
-    this.length--;
-    let value = this.list[this.length];
-    delete this.list[this.length];
-    return value;
+  console.log = function (func, value) {
+    let message = getTime();
+    if (func === push) {
+      message += ' Добавлено новое значение: ' + value;
+    } else if (func === pop) {
+      message += ' Удалено значение: ' + value;
+    } else if (func === peek) {
+      message += ' Показано значение: ' + value;
+    } else if (func === getSize) {
+      message += ' Показана длина стека: ' + value;
+    }
+    originalLog.call(this, message);
+    addLogElement(message);
+  };
+
+  // Реализация стека
+
+  class Stack {
+    constructor() {
+      this.list = {};
+      this.length = 0;
+    }
+
+    push(value) {
+      this.list[this.length] = value;
+      this.length++;
+      console.log(push, value);
+    }
+
+    pop() {
+      if (this.length === 0) return undefined;
+      this.length--;
+      let value = this.list[this.length];
+      delete this.list[this.length];
+      console.log(pop, value);
+      return value;
+    }
+
+    peek() {
+      console.log(peek, this.list[this.length - 1]);
+      return this.list[this.length - 1];
+    }
+
+    getSize () {
+      console.log(getSize, this.length);
+      return this.length;
+    }
   }
 
-  peek() {
-    return this.list[this.length - 1];
-  }
+  // Создание стека
 
-  getSize () {
-    return this.length;
-  }
-}
+  let myStack = new Stack();
 
-// Создание стека
+  // Создание слушателей событий
 
-let myStack = new Stack();
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
 
-// Создание слушателей событий
+    myStack.push(textInput.value);
+    textInput.value = '';
 
-form.addEventListener('submit', function (evt) {
-  evt.preventDefault();
+    popButton.disabled = false;
+    peekButton.disabled = false;
+  });
 
-  myStack.push(textInput.value);
-  textInput.value = "";
+  getSizeButton.addEventListener('click', function () {
+    textOutput.textContent = myStack.getSize();
+  });
 
-  popButton.disabled = false;
-  peekButton.disabled = false;
-});
+  popButton.addEventListener('click', function () {
+    textOutput.textContent = myStack.pop();
 
-getSizeButton.addEventListener('click', function () {
-  textOutput.textContent = myStack.getSize();
-});
+    if (myStack.length === 0) {
+      popButton.disabled = true;
+      peekButton.disabled = true;
+    }
+  });
 
-popButton.addEventListener('click', function () {
-  textOutput.textContent = myStack.pop();
+  peekButton.addEventListener('click', function () {
+    textOutput.textContent = myStack.peek();
+  });
 
-  let length = myStack.getSize();
-
-  if (length === 0) {
-    popButton.disabled = true;
-    peekButton.disabled = true;
-  }
-});
-
-peekButton.addEventListener('click', function () {
-  textOutput.textContent = myStack.peek();
-});
+})();
